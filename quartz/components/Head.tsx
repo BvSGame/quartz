84022c885a4143574c1f94523a33c7d1660ed002
liledix4 <ed@liledix.com>
2026-03-20 21:01:58 +0000

    20 March 2026, no.1: Bing Webmaster Tools verification

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -86,6 +86,7 @@ export default (() => {
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
         <meta name="google-site-verification" content="yHZUqprpX4vT01MHxQY_Wvzz3HWLV1Z4RxMC9o4o2h8" />
+        <meta name="msvalidate.01" content="AFC5D721E174ABF563ACAB1495BEFA37" />
 
         {css.map((resource) => CSSResourceToStyleElement(resource, true))}
         {js
